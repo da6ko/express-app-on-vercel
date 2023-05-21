96fbd5e3ee2e76c3c878f9cca5f16a1f99829ab7
da6ko <106763689+da6ko@users.noreply.github.com>
2023-05-21 17:59:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+// Configure Express to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+// MongoDB connection URI and database name
+const uri = 'mongodb+srv://user1:user1@testcluster.ehr4d.mongodb.net/Locations?retryWrites=true&w=majority';
+const dbName = 'Locations';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDBAtlas() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error(error);
+  }
+}
+connectToMongoDBAtlas();
+
+// Endpoint to save data to MongoDB
+app.post('/saveData', (req, res) => {
+
+  const data = req.body;
+
+  console.log('Received data:', data);
+
+  const db = client.db(dbName);
+  const collection = db.collection('Users');
+
+  // Insert the data into MongoDB
+  collection.insertOne(data, function(err, result) {
+    if (err) {
+      console.error('Error saving data to MongoDB:', err);
+      res.status(500).json({ error: 'Failed to save the data' });
+    } else {
+      console.log('Data saved successfully');
+      res.json({ success: true });
+    }
+  });
 });
 
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(8080, () => {
-  console.log("Running on port 8080...");
+  console.log('Server started on port 8080');
 });
+
 
 // Export the Express API
 module.exports = app;
